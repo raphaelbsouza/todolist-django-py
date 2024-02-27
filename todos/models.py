0813f2ee32573e11ea8bf613581d8b9de73219cc
Raphael Souza <raphaelstc@msn.com>
2024-02-27 01:07:54 -0300

    Add To do. Lista de Tarefas

--- todos/models.py
+++ todos/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(
+        verbose_name="Titulo", max_length=100, null=False, blank=False
+    )
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateTimeField(null=False, blank=False)
+    deadline = models.DateTimeField(
+        verbose_name="Data de Entrega", null=False, blank=False
+    )
     finish_at = models.DateTimeField(null=True)
