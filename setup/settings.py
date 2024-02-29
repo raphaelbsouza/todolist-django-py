6443eddade3a774e8775d7c3c6d85c8e90e1560d
Raphael Souza <raphaelstc@msn.com>
2024-02-29 09:19:59 -0300

    updates forms, background

--- setup/settings.py
+++ setup/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config, Csv
 from dj_database_url import parse as db_url
@@ -116,3 +117,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
